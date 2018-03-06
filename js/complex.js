faa1d63f47710a93efacf3d29c6f28bc56c456b4
JamesMWeller <33237253+JamesMWeller@users.noreply.github.com>
2018-03-06 14:19:30 +0000

    Add files via upload

--- js/complex.js
+++ js/complex.js
@@ -1,5 +1,12 @@
 ﻿var complexState = {
 	create: function (){
+		
+		this.player = JSON.parse(localStorage.getItem('planet')) || {
+			air: 0,
+			water: 1,
+		};
+		
+		
 		var simpleButton = createButton(100, game.height - 50, 150, 50, "Home");
 	    simpleButton.events.onInputUp.add(
 			function(){
--- js/simple.js
+++ js/simple.js
@@ -3,64 +3,120 @@
 	// var is used to create variables that are only used within the current function (local scope) 
 	create: function (){
 
+		
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+
+		asteroids = game.add.physicsGroup(Phaser.Physics.ARCADE);
+		planets = game.add.physicsGroup(Phaser.Physics.ARCADE);
+
+		planets.enableBody = true;
+		asteroids.enableBody = true;
+
+		game.input.addPointer();
+		game.input.addPointer();
+		game.input.addPointer();
+		game.input.addPointer();
+
+		text = game.add.text(16, 16, 'Your Planet has ' + planetLife + ' lives left.', { fill: '#ffffff' });
+
+
+		for (var i = 0; i < 12; i++) {
+			asteroid = asteroids.create(game.world.randomX, game.world.randomY, 'asteroid')
+			{
+				this.worldType = this.randomElement();
+				enableBody = true;
+				game.physics.enable(asteroid, Phaser.Physics.ARCADE);
+			};
+
+			var rand = game.rnd.realInRange(1, 2);
+			asteroid.scale.setTo(rand, rand);
+			asteroid.body.setCircle();
+			asteroid.body.collideWorldBounds = true;
+			asteroid.inputEnabled = true;
+			asteroid.input.enableDrag();
+			asteroid.input.useHandCursor = true;
+			asteroid.events.onDragStart.add(this.onDragStart, asteroid);
+			asteroid.events.onDragStop.add(this.onDragStop, asteroid);
+
+		}
+
+		for (var i = 0; i < 1; i++) {
+			planet = planets.create(game.width / 2, game.height / 2, 'planet');
+			planet.scale.setTo(0.3, 0.3);
+			planet.body.setCircle(345, 280, 375);
+			planet.body.collideWorldBounds = true;
+			planet.inputEnabled = true;
+			planet.input.enableDrag();
+			planet.input.useHandCursor = true;
+		}
+		
 		var simpleButton = createButton(100, game.height - 50, 150, 50, "Home");
 	    simpleButton.events.onInputUp.add(
 			function(){
 				game.state.start('createMedium');
 			},this
 		)
-		game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    asteroids = game.add.physicsGroup(Phaser.Physics.ARCADE);
-    planets = game.add.physicsGroup(Phaser.Physics.ARCADE);
-
-    planets.enableBody = true;
-    asteroids.enableBody = true;
-
-    game.input.addPointer();
-    game.input.addPointer();
-    game.input.addPointer();
-    game.input.addPointer();
-
-    text = game.add.text(16, 16, 'Your Planet has ' + planetLife + ' lives left.', { fill: '#ffffff' });
-
-
-    for (var i = 0; i < 12; i++) {
-        asteroid = asteroids.create(game.world.randomX, game.world.randomY, 'asteroid')
-        {
-            this.worldType = randomElement();
-            enableBody = true;
-            game.physics.enable(asteroid, Phaser.Physics.ARCADE);
-        };
-
-        var rand = game.rnd.realInRange(1, 2);
-        asteroid.scale.setTo(rand, rand);
-        asteroid.body.setCircle();
-        asteroid.body.collideWorldBounds = true;
-        asteroid.inputEnabled = true;
-        asteroid.input.enableDrag();
-        asteroid.input.useHandCursor = true;
-        //asteroid.events.onDragStart.add(onDragStart, asteroid);
-        //asteroid.events.onDragStop.add(onDragStop, asteroid);
-
-    }
-
-    for (var i = 0; i < 1; i++) {
-        planet = planets.create(game.width / 2, game.height / 2, 'planet');
-        planet.scale.setTo(0.3, 0.3);
-        planet.body.setCircle(345, 280, 375);
-        planet.body.collideWorldBounds = true;
-        planet.inputEnabled = true;
-        planet.input.enableDrag();
-        planet.input.useHandCursor = true;
-    }
 	},
 	update: function (){
-		   
+		
+		 while (pendingDestroy.length > 0) {
+        pendingDestroy.pop().destroy();
+    }
+    if (game.physics.arcade.overlap(asteroids, planets, this.planetLoseLife)) {
+        text.text = 'Your Planet has ' + planetLife + ' lives left.';
+    }
+
+    game.physics.arcade.overlap(asteroids, asteroids, this.addToPendingDestroy);
+
+		
 	},
 	
 	randomElement: function(){
 		
 	},
+	
+	onDragStart: function (){
+		this.dragStartLocation = { x: this.x, y: this.y };
+		console.log(this.dragStartLocation);
+	},
+	
+	onDragStop: function (){
+		this.dragStopLocation = { x: this.x, y: this.y };
+		console.log(this.dragStopLocation.y);
+		var x = this.dragStopLocation.x + (this.dragStopLocation.x - this.dragStartLocation.x);
+		var y = this.dragStopLocation.y + (this.dragStopLocation.y - this.dragStartLocation.y);
+		console.log("dx = " + x);
+		console.log("dy = " + y);
+		game.physics.arcade.moveToXY(this, x, y);
+	},
+	
+	addToPendingDestroy: function (a,b){
+		pendingDestroy.push(a);
+		pendingDestroy.push(b);
+	},
+	
+	overlapDestroy: function (a, b) {
+
+    var boundsA = a.getBounds();
+    var boundsB = b.getBounds();
+
+    var x = (a.x + b.x) / 2;
+    var y = (a.y + b.y) / 2;
+	
+	},
+	
+	
+	
+	planetLoseLife: function(a, b)
+	{
+		if (planetLife == 0) {
+				alert("Game Over!!");
+				location.reload();
+		} else if (planetLife > 0) {
+			pendingDestroy.push(a)
+			planetLife = planetLife - 1;
+		}
+	},
+	
 	
 }
